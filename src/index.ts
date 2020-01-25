2960bdd539747914978d93687c7f19489ee1cf09
so0oshiance <mohamad.farhany@gmail.com>
2020-01-25 18:38:38 +0330

    sdasd

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 
 const app = express();
-const getYear = (date: Date): string => `${date.getFullYear()}`;
+//const getYear = (date: Date): string => `${date.getFullYear()}`;
 app.get('/', (req, res) => {
-  const year = getYear(new Date());
-  res.send(`Hello World ! ${year}`);
+  // const year = getYear(new Date());
+  res.send(`Hello World !`);
 });
 app.listen(3000, () => {
   console.log('test');
