874ee32be3bb9b4841ee43c93052852e9c0e57ba
so0oshiance <mohamad.farhany@gmail.com>
2020-01-25 18:39:02 +0330

    sdasd

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 
 const app = express();
-//const getYear = (date: Date): string => `${date.getFullYear()}`;
+// const getYear = (date: Date): string => `${date.getFullYear()}`;
 app.get('/', (req, res) => {
   // const year = getYear(new Date());
-  res.send(`Hello World !`);
+  res.send('Hello World !');
 });
 app.listen(3000, () => {
-  console.log('test');
-
-  console.log('App listening on port 3000!');
+  //console.log('test');
+  //console.log('App listening on port 3000!');
 });
